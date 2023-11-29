e890d8a0b78983a1ae79b1379fe30646fd0f8be5
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-11-29 10:53:24 +0300

    edit

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,7 +10,8 @@ const Profile: React.FC = () => {
           <div className="row justify-content-around mt-5 user-info">
               <div className="col-12 col-md-3">
                   <figure className='avatar avatar-profile'>
-                      <img className="rounded-circle img-fluid" src='' alt=''/>
+                      <img className="rounded-circle img-fluid"   src={`https://picsum.photos/200/300`}
+                           alt={`The image alt tag for the big image`}/>
                   </figure>
                   <a href="#" id="edit_profile" className="btn btn-primary btn-block my-5">
                       Edit Profile
