38362cd3d9d12c6c90cc20e3682957687e6072d7
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-11-29 10:46:30 +0300

    edit

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,28 +5,35 @@ const Profile: React.FC = () => {
   const currentUser = getCurrentUser();
 
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>{currentUser.username}</strong> Profile
-        </h3>
-      </header>
-      <p>
-        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-      </p>
-      <p>
-        <strong>Id:</strong> {currentUser.id}
-      </p>
-      <p>
-        <strong>Email:</strong> {currentUser.email}
-      </p>
-      <strong>Authorities:</strong>
-      <ul>
-        {currentUser.roles &&
-          currentUser.roles.map((role: string, index: number) => <li key={index}>{role}</li>)}
-      </ul>
-    </div>
+      <>
+          <h2 className="mt-5 ml-5">My Profile</h2>
+          <div className="row justify-content-around mt-5 user-info">
+              <div className="col-12 col-md-3">
+                  <figure className='avatar avatar-profile'>
+                      <img className="rounded-circle img-fluid" src='' alt=''/>
+                  </figure>
+                  <a href="#" id="edit_profile" className="btn btn-primary btn-block my-5">
+                      Edit Profile
+                  </a>
+              </div>
+
+              <div className="col-12 col-md-5">
+                  <h4>Full Name</h4>
+                  <p>Ghulam Abbas</p>
+
+                  <h4>Email Address</h4>
+                  <p>ghulamabbas258@gmail.com</p>
+
+                  <a href="#" className="btn btn-danger btn-block mt-5">
+                      My Orders
+                  </a>
+
+                  <a href="#" className="btn btn-primary btn-block mt-3">
+                      Change Password
+                  </a>
+              </div>
+          </div>
+      </>
   );
 };
 
