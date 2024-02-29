c2f34bdf4e6019a1ee9800428d9f91bc9a301f05
Soslan Aldatov <soslanaldatov@gmail.com>
2024-02-29 17:01:28 +0400

    update API URL

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,17 @@
-export const API_URL: string = "http://localhost:8099/api/auth/";
-export const API_URL_TEST: string = "http://localhost:8099/api/test/";
+export const API_URL: string = "https://api.edtech.sptm.dev/auth/";
+export const API_URL_TEST: string = "https://api.edtech.sptm.dev/test/";
 
 export const urls = {
-    main: '/',
-    login: '/login',
-    profile: '/profile',
-    register: "/register",
-    news: "/news",
-    about: '/about',
-    ads: '/ads',
-    user: '/user',
-    mod: '/mod',
-    admin: '/admin',
-    addNews: '/addNews',
-    addAds: '/addAds',
-}
+  main: "/",
+  login: "/login",
+  profile: "/profile",
+  register: "/register",
+  news: "/news",
+  about: "/about",
+  ads: "/ads",
+  user: "/user",
+  mod: "/mod",
+  admin: "/admin",
+  addNews: "/addNews",
+  addAds: "/addAds",
+};
