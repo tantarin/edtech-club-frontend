51768483cad8e5ee69d1a282b3ba8cfb8e2286fc
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-03-30 20:03:56 +0300

    правки

--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { FiSearch } from 'react-icons/fi';
 import ProjectSingle from './ProjectSingle';
 import ProjectsFilter from './ProjectsFilter';
 import {ProjectsContext} from "./ProjectContext";
@@ -45,20 +44,6 @@ const ProjectsGrid: React.FC = () => {
                         "
                 >
                     <div className="flex justify-between gap-2">
-						<span
-                            className="
-                                hidden
-                                sm:block
-                                bg-primary-light
-                                dark:bg-ternary-dark
-                                p-2.5
-                                shadow-sm
-                                rounded-xl
-                                cursor-pointer
-                                "
-                        >
-							<FiSearch className="text-ternary-dark dark:text-ternary-light w-5 h-5"></FiSearch>
-						</span>
                         <input
                             onChange={(e) => {
                                 setSearchProject(e.target.value);
@@ -85,6 +70,15 @@ const ProjectsGrid: React.FC = () => {
                             placeholder="Search Projects"
                             aria-label="Name"
                         />
+                        <button type="submit"
+                                className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                 viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                      stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                            <span className="sr-only">Search</span>
+                        </button>
                     </div>
 
                     <ProjectsFilter setSelectProject={setSelectProject} />
