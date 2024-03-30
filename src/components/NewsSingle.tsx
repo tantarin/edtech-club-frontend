4025daf22f9a223b38aa336366df68feb9fdc076
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-03-30 23:49:43 +0300

    правки

--- src/components/NewsSingle.tsx
+++ src/components/NewsSingle.tsx
@@ -8,25 +8,28 @@ interface ProjectSingleProps {
 
 const NewsSingle = ({ title, content, image }: ProjectSingleProps) => {
     return (
-        <div className="transform  rounded-xl bg-white shadow-xl transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer dark:bg-ternary-dark">
+        <div className="transform rounded-xl bg-white shadow-xl transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer dark:bg-ternary-dark relative">
             <Link to="/projects/single-project" aria-label="Single Project">
-                    <div>
+                <div>
                     <img
-                            src={image}
-                            className="rounded-t-xl border-none"
-                            alt="Single Project"
-                        />
-                    </div>
-                    <div className="text-left px-4 py-6">
-                        <p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light mb-2">
+                        src={image}
+                        className="rounded-t-xl border-none"
+                        alt="Single Project"
+                    />
+                </div>
+                <div className="text-left px-4 py-6">
+                    <p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light mb-2 line-clamp-1">
                         {title}
-                        </p>
-                        <p className="mb-4 text-base line-clamp-3">
-                            {content}
-                        </p>
-                    </div>
+                    </p>
+                    <p className="mb-4 text-base line-clamp-3">
+                        {content}
+                    </p>
+                </div>
             </Link>
-         </div>
+            <div className="border-t-2 border-neutral-100 px-6 py-2 text-surface/75 dark:border-white/10 dark:text-neutral-300 absolute bottom-0 w-full">
+                <div className="mt-auto">2 days ago</div>
+            </div>
+        </div>
     );
 };
 
