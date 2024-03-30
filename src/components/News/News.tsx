33141e11b05a78cff2614c5fa70fd73c7074ee78
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-03-30 23:01:20 +0300

    правки

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,115 +1,60 @@
-import React, { useState, useEffect } from "react";
-
-import { getNews } from "../../services/user.service";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea, Button, Box, Grid } from "@mui/material";
-import { Link } from "react-router-dom";
-import { getCurrentUser } from "../../services/auth.service";
+import React, {useState, useEffect} from "react";
+import {getNews} from "../../services/user.service";
+import {getCurrentUser} from "../../services/auth.service";
 import IUser from "../../types/user.type";
 import "./News.css";
-import SearchForm from "../SearchForm";
+import Image1 from "../../assets/img/ui-project-1.jpg";
+import NewsSingle from "../NewsSingle";
 
 interface IContent {
-  content: string;
-  date: string;
-  header: string;
-  id: number;
+    content: string;
+    date: string;
+    header: string;
+    id: number;
 }
 
 const News: React.FC = () => {
-  const [content, setContent] = useState<IContent[]>([]);
-  const [user, setUser] = useState<IUser | undefined>(() => getCurrentUser());
+    const [content, setContent] = useState<IContent[]>([]);
+    const [user, setUser] = useState<IUser | undefined>(() => getCurrentUser());
 
-  useEffect(() => {
-    loadNews();
-    getCurrentUser();
-  }, []);
+    useEffect(() => {
+        loadNews();
+    }, []);
 
-  const loadNews = () => {
-    getNews().then(
-      response => {
-        setContent(response.data);
-      },
-      error => {
-        const _content =
-          (error.response && error.response.data) ||
-          error.message ||
-          error.toString();
+    const loadNews = () => {
+        getNews()
+            .then(response => {
+                setContent(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching news:", error);
+            });
+    };
 
-        setContent(_content);
-      }
+    return (
+        <>
+            <div className="showcase2">
+                <div className="overlay px-5">
+                    <h1 className="text-4xl font-bold text-white text-center mb-4 lg:text-6xl">
+                        События клуба
+                    </h1>
+                </div>
+            </div>
+            {/*Card*/}
+            <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+                <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
+                    {content.map(item => (
+                        <NewsSingle
+                            title={item.header}
+                            category={item.content}
+                            image='https://images.pexels.com/photos/61180/pexels-photo-61180.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500'
+                            key={item.id}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
     );
-  };
-
-  return (
-      <>
-        <div className="showcase2">
-          <div className="overlay px-5">
-            <h1 className="text-4xl font-bold text-white text-center mb-4 lg:text-6xl">
-              События клуба
-            </h1>
-          </div>
-        </div>
-    <div className="container mt-3">
-      <div>
-        <Box
-          m={1}
-          display="flex"
-          justifyContent="flex-end"
-          alignItems="flex-end"
-        >
-          {user?.roles && user.roles.includes("ROLE_ADMIN") && (
-            <Link to="/addNews" style={{ textDecoration: "none" }}>
-              <Button variant="contained" color="primary" sx={{ height: 40 }}>
-                Добавить новость
-              </Button>
-            </Link>
-          )}
-        </Box>
-        <Grid container spacing={3} className={"px-0"}>
-          {Array.isArray(content) &&
-            content?.map(item => {
-              return (
-                <Grid item xs={12} sm={6} md={4}>
-                  <Card
-                    key={item.id}
-                    sx={{ margin: "0 2px 2px 0", maxHeight: 500 }}
-                    style={{ height: "100%" }}
-                  >
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image="https://via.placeholder.com/345x140"
-                        alt="Placeholder Image"
-                      />
-                      <CardContent>
-                        <Typography
-                          gutterBottom
-                          variant="h5"
-                          component="div"
-                          className={"header"}
-                          title={item.header}
-                        >
-                          {item.header}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          {item.content}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              );
-            })}
-        </Grid>
-      </div>
-    </div>
-      </>
-  );
 };
 
 export default News;
---
+++ src/components/NewsSingle.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+
+interface ProjectSingleProps {
+    title: string;
+    category: string;
+    image: string;
+}
+
+const NewsSingle = ({ title, category, image }: ProjectSingleProps) => {
+    return (
+        <div className="transform  rounded-xl bg-white shadow-xl transition duration-300 hover:scale-105">
+            <Link to="/projects/single-project" aria-label="Single Project">
+                <div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
+                    <div>
+                    <img
+                            src={image}
+                            className="rounded-t-xl border-none"
+                            alt="Single Project"
+                        />
+                    </div>
+                    <div className="text-center px-4 py-6">
+                        <p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light mb-2">
+                        {title}
+                        </p>
+                        <p className="mb-4 text-base">
+                            {category}
+                        </p>
+                    </div>
+                </div>
+            </Link>
+         </div>
+    );
+};
+
+export default NewsSingle;
