963018c2a23010bdc2683c51c6476c5aee454375
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-03-30 14:08:50 +0300

    правки

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,10 +1,16 @@
+import WebImage1 from '../assets/img/web-project-1.jpg';
+import WebImage2 from '../images/web-project-2.jpg';
+import MobileImage1 from '../assets/img/mobile-project-1.jpg';
+import MobileImage2 from '../assets/img/mobile-project-2.jpg';
+import UIImage1 from '../assets/img/ui-project-1.jpg';
+import UIImage2 from '../assets/img/ui-project-1.jpg';
 
 export const projectsData = [
     {
         id: 1,
         title: 'Google Health Platform',
         category: 'Web Application',
-        img: 'https://random.imagecdn.app/500/150',
+        img: MobileImage1,
         ProjectHeader: {
             title: 'Project Management UI - From Context',
             publishDate: 'Jul 26, 2021',
@@ -15,13 +21,13 @@ export const projectsData = [
         id: 2,
         title: 'Phoenix Digital Agency',
         category: 'Mobile Application',
-        img: 'https://random.imagecdn.app/500/150',
+        img: MobileImage2,
     },
     {
         id: 3,
         title: 'Project Management UI',
         category: 'UI/UX Design',
-        img: 'https://random.imagecdn.app/500/150',
+        img: WebImage1,
     },
     {
         id: 4,
