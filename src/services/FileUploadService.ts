ecd82c215e981f3e51ebf4636569dadbc1c2bb50
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-03-31 11:56:16 +0300

    правки

---
+++ src/services/FileUploadService.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const upload = (file: File, onUploadProgress: any): Promise<any> => {
+    let formData = new FormData();
+
+    formData.append("file", file);
+
+    return axios.post("/upload", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress,
+    });
+};
+
+const getFiles = () : Promise<any> => {
+    return axios.get("/files");
+};
+
+const FileUploadService = {
+    upload,
+    getFiles,
+};
+
+export default FileUploadService;
