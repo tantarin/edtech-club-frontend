51779169517a30bae11bd979b695f9019d7f417b
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-04-02 18:46:20 +0300

    теги

--- src/views/ads.tsx
+++ src/views/ads.tsx
@@ -1,49 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { Container, Button } from "@mui/material";
-import { Link } from "react-router-dom";
-import IUser from "../types/user.type";
-import { getCurrentUser } from "../services/auth.service";
-import { getAds } from "../services/user.service";
+import React, {useEffect, useState} from 'react';
 import SingleAd from "../components/Ads/SingleAd";
 import {deleteAd} from "../services/ads.service";
+import {getAds} from "../services/user.service";
+import {Link} from "react-router-dom";
+import {Container, Button} from "@mui/material";
 
 interface AdData {
     id: bigint;
     header: string;
     content: string;
+    category: string; // Добавлена категория для объявления
 }
-
 
 const Ads: React.FC = () => {
     const [content, setContent] = useState<AdData[]>([]);
-    const [user, setUser] = useState<IUser | undefined>(() => getCurrentUser());
+    const [categories, setCategories] = useState(["Java", "Python", "Тестирование", "React"]);
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // Исправлен тип для selectedCategories
+    const [filteredAdsList, setFilteredAdsList] = useState<AdData[]>([]); // Переименована переменная и изменен тип
 
     const loadAds = () => {
         getAds().then(
             (response) => {
                 setContent(response.data);
+                setFilteredAdsList(response.data);
             },
             (error) => {
+                console.error("Error2:", error.message);
                 const _content =
                     (error.response && error.response.data) ||
                     error.message ||
                     error.toString();
-                const errorId = error.response ? error.response.data.id : null;
-                setContent([{ id: errorId, header: "Error", content: _content }]);
+                // const errorId = error.response ? error.response.data.id : null;
+                const errorId = null;
+                setContent([{id: BigInt(1), header: "Error", content: _content, category: "Java"}]);
             }
         );
     };
 
     const handleDelete = async (id: bigint) => {
         try {
-            await deleteAd(id); // Попытка удалить объявление
-            // Если удаление прошло успешно, обновляем состояние content
+            await deleteAd(id);
             setContent(prevContent => prevContent.filter(ad => ad.id !== id));
+            setFilteredAdsList(prevContent => prevContent.filter(ad => ad.id !== id)); // Удаление объявления из отфильтрованного списка
             console.log("Ad deleted successfully");
         } catch (error) {
             console.error("Error deleting ad:", error);
         }
     };
+
+    const addCategory = (category: string) => { // Указание типа для параметра
+        if (!selectedCategories.includes(category)) {
+            setSelectedCategories(prev => ([...prev, category]))
+        }
+    }
+
+    const removeCategory = (category: string) => { // Указание типа для параметра
+        if (selectedCategories.includes(category)) {
+            const removedList = selectedCategories.filter((item) => (item !== category));
+            setSelectedCategories(removedList);
+        }
+    }
+
+    const resetCategory = () => {
+        setSelectedCategories([]);
+    }
+
+    useEffect(() => {
+        if (selectedCategories.length === 0) {
+            setFilteredAdsList(content); // Установка отфильтрованного списка в начальное значение при сбросе категорий
+        } else {
+            setFilteredAdsList(content.filter((item) => (selectedCategories.includes(item.category))));
+        }
+    }, [selectedCategories, content]);
 
     useEffect(() => {
         loadAds();
@@ -53,22 +81,45 @@ const Ads: React.FC = () => {
         <div className="container mt-3">
             <Container>
                 <div className="gap-4 flex flex-col mt-2">
-                    {/*{user?.roles && user.roles.includes("ROLE_ADMIN") && (*/}
                     {(
-                        <Link to="/addAds" style={{ textDecoration: "none" }}>
-                            <Button variant="contained" color="primary" sx={{ height: 50 }}>
+                        <Link to="/addAds" style={{textDecoration: "none"}}>
+                            <Button variant="contained" color="primary" sx={{height: 50}}>
                                 Добавить объявление
                             </Button>
                         </Link>
                     )}
-                    <div className="grid grid-cols-1 gap-4">
-                        {Array.isArray(content) &&
-                            content.map((ad, index) => <SingleAd key={index} {...ad} handleDelete={handleDelete}/>)}
-                    </div>
+                        <div className='w-full h-[90%] rounded-md bg-white'>
+                            <div className='relative w-full h-[15%] flex items-center overflow-x-auto'>
+                                <span className='mx-3 ml-5 font-medium'> Categories: </span>
+                                {categories.map((category) => (
+                                    <div
+                                        onClick={() => {
+                                            if (selectedCategories.includes(category)) {
+                                                removeCategory(category);
+                                            } else {
+                                                addCategory(category);
+                                            }
+                                        }}
+                                        className={`w-fit min-w-fit h-8 mx-2 px-5 py-2 flex flex-row justify-center items-center text-sm border break-keep rounded-3xl cursor-pointer transition-all duration-300 ${(selectedCategories.includes(category)) ? 'border-blue-500 bg-blue-500 text-white' : ' border-gray-500 bg-white text-gray-900'} `}>
+                                        {category}
+                                    </div>
+                                ))}
+                                <div
+                                    onClick={() => resetCategory()}
+                                    className={`${(selectedCategories.length > 0) ? 'opacity-100' : 'opacity-0 pointer-events-none'} sticky right-0 w-fit h-full px-5 flex justify-center items-center text-blue-500 bg-white backdrop-blur-lg cursor-pointer hover:text-blue-700 transition-all duration-300`}
+                                >
+                                    clear
+                                </div>
+                            </div>
+                        </div>
+                            <div className="grid grid-cols-1 gap-4">
+                                {content.map((ad, index) => <SingleAd key={index} {...ad}
+                                                                              handleDelete={handleDelete}/>)}
+                            </div>
                 </div>
             </Container>
         </div>
-    );
-};
+    )
+}
 
 export default Ads;
