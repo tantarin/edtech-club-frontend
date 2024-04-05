3f2bc592205440b9dfe9f27b7b59c0b25f04cf49
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-04-06 01:29:32 +0300

    убрано лишнее в accel

--- src/views/accelerator.tsx
+++ src/views/accelerator.tsx
@@ -104,66 +104,6 @@ const Accel: React.FC = () => {
                                 </div>
                             </div>
                         </div>
-
-
-                        <div className="flex flex-wrap items-center mt-32">
-                            <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
-                                <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100">
-                                    <i className="fas fa-user-friends text-xl"></i>
-                                </div>
-                                <h3 className="text-3xl mb-2 font-semibold leading-normal">
-                                    Сотрудничество с нами — это новые возможности
-                                </h3>
-                                <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                                    Мы создаем уникальные возможности для вашего стартапа, чтобы помочь вам расти и достигать новых высот в вашем бизнесе.
-                                </p>
-                                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
-                                    Наш акселератор предоставляет не только знания и финансовую поддержку, но и ценные контакты, менторство и ресурсы, необходимые для вашего успеха.
-                                </p>
-                                <a
-                                    href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
-                                    className="font-bold text-gray-800 mt-8"
-                                >
-                                    Узнать больше о нашем акселераторе!
-                                </a>
-                            </div>
-
-                            <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
-                                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-600">
-                                    <img
-                                        alt="..."
-                                        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
-                                        className="w-full align-middle rounded-t-lg"
-                                    />
-                                    <blockquote className="relative p-8 mb-4">
-                                        <svg
-                                            preserveAspectRatio="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 583 95"
-                                            className="absolute left-0 w-full block"
-                                            style={{
-                                                height: "95px",
-                                                top: "-94px"
-                                            }}
-                                        >
-                                            <polygon
-                                                points="-30,95 583,95 583,65"
-                                                className="text-pink-600 fill-current"
-                                            ></polygon>
-                                        </svg>
-                                        <h4 className="text-xl font-bold text-white">
-                                            Top Notch Services
-                                        </h4>
-                                        <p className="text-md font-light mt-2 text-white">
-                                            The Arctic Ocean freezes every winter and much of the
-                                            sea-ice then thaws every summer, and that process will
-                                            continue whatever happens.
-                                        </p>
-                                    </blockquote>
-                                </div>
-                            </div>
-
-                        </div>
                     </div>
                 </section>
 
