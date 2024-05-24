8933cf58b6d847dd59bf613dd9fe7ab834a379f5
Dmitriy Schukin <schukindmitriy@gmail.com>
2024-05-24 17:57:40 +0200

    change root renderer for new react 18+

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './styles/index.css';
 import './styles/color.css'
@@ -7,11 +8,13 @@ import './styles/output.css'
 import App from './App';
 import './css/main.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!)
+
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
