1eaeb72440e0dadc9f6eb6836f49e51d49c01a7f
Dmitriy Schukin <schukindmitriy@gmail.com>
2024-05-27 15:40:18 +0200

    fixed React render method and styles hierarchy

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import './styles/output.css'
 import './styles/index.css';
 import './styles/color.css'
-import './styles/output.css'
 import App from './App';
 import './css/main.css';
 
